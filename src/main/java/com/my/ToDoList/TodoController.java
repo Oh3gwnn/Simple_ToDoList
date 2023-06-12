9bd215c411b60d36e75fa9fef382da207d069df9
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-12 11:54:18 +0900

    feat: Create a basic structure  - add Todo Dto, Controller, Service, todo.html

---
+++ src/main/java/com/my/ToDoList/TodoController.java
@@ -0,0 +1,28 @@
+package com.my.ToDoList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TodoController {
+
+    @GetMapping("/todo")
+    public String home(){
+        return "todo";
+    }
+
+    public String create() {
+        // 새로운 TODO를 생성하는 컨트롤러 메소드
+        throw new RuntimeException("TODO");
+    }
+
+    public String update() {
+        // TODO의 done 상태를 변경하는 메소드
+        throw new RuntimeException("TODO");
+    }
+
+    public String delete() {
+        // TODO를 삭제하는 메소드
+        throw new RuntimeException("TODO");
+    }
+}
---
+++ src/main/java/com/my/ToDoList/TodoService.java
@@ -0,0 +1,7 @@
+package com.my.ToDoList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+}
