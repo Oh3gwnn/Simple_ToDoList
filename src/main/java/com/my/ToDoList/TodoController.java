82fc8938163f403c1cb0f275d0cacebf9a9720d2
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-24 23:06:44 +0900

    feat: modified TodoController

--- src/main/java/com/my/ToDoList/TodoController.java
+++ src/main/java/com/my/ToDoList/TodoController.java
@@ -1,19 +1,37 @@
 package com.my.ToDoList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
 
-    @GetMapping("/todo")
-    public String home(){
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping(value = "/todo")
+    public String todo(Model model) {
+        model.addAttribute("todoList", todoService.readAll());
         return "todo";
     }
 
-    public String create() {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/create")
+    public String create(
+            @RequestParam("todo-desc") String content) {
         // 새로운 TODO를 생성하는 컨트롤러 메소드
-        throw new RuntimeException("TODO");
+        System.out.println(content);
+        TodoDto newTodo = todoService.createToDo(content);
+        System.out.println(newTodo.toString());
+        return "redirect:/todo";
     }
 
     public String update() {
