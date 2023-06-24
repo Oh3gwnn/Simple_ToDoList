feeb3e7949fbcd47f07cfd78feb3764a69eb0f9e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-06-24 23:06:35 +0900

    feat: modified TodoService

--- src/main/java/com/my/ToDoList/TodoService.java
+++ src/main/java/com/my/ToDoList/TodoService.java
@@ -2,6 +2,22 @@ package com.my.ToDoList;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TodoService {
+    private final List<TodoDto> todoList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public TodoDto createToDo(String content) {
+        TodoDto newTodo = new TodoDto(nextId, content, false);
+        nextId++;
+        todoList.add(newTodo);
+        return newTodo;
+    }
+
+    public List<TodoDto> readAll() {
+        return todoList;
+    }
 }
